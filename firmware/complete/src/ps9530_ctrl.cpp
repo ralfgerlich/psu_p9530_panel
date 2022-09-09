379ea43b3fa6e18585de4720136658870b97a25c
Ralf Gerlich <r.gerlich@t-online.de>
2022-09-09 18:58:24 +0200

    Streamline generation of ADC calibration tables

--- firmware/complete/src/ps9530_ctrl.cpp
+++ firmware/complete/src/ps9530_ctrl.cpp
@@ -27,7 +27,7 @@
 
 PS9530_Ctrl PS9530_Ctrl::instance;
 
-PS9530_Ctrl& PS9530_Ctrl::getInstance() {
+PS9530_Ctrl &PS9530_Ctrl::getInstance() {
     return instance;
 }
 
@@ -36,8 +36,8 @@ PS9530_Ctrl::PS9530_Ctrl():
     rawDACValue{0, 0},
     currentMuxChannel(muxChannel_voltage),
     overTempMode(0),
-    rawADCMeasurements{0,0,0,0},
-    currentADCState(adcChannel__idle<<1)
+    rawADCMeasurements{0, 0, 0, 0},
+    currentADCState(adcChannel__idle << 1)
 {
 }
 
@@ -56,11 +56,11 @@ void PS9530_Ctrl::init() {
     /* We set the timer interrupt to about 100Hz.
      * Clear-Timer-on-Compare (CTC) with fCPU/1024 and OCRA = 155
      * => f = 16MHZ/1024/156 = 100Hz */
-    TCCR0A = (1<<WGM01)|(0<<WGM00);
-    TCCR0B = (0<<WGM02)|(1<<CS02)|(0<<CS01)|(1<<CS00);
+    TCCR0A = (1 << WGM01) | (0 << WGM00);
+    TCCR0B = (0 << WGM02) | (1 << CS02) | (0 << CS01) | (1 << CS00);
     OCR0A = 49;
-    TIMSK0 = (1<<OCIE0A);
-    TIFR0 = (1<<OCIE0A);
+    TIMSK0 = (1 << OCIE0A);
+    TIFR0 = (1 << OCIE0A);
 }
 
 KeyCode PS9530_Ctrl::readKeycode() {
@@ -74,9 +74,9 @@ void PS9530_Ctrl::update() {
 }
 
 void PS9530_Ctrl::updateADC() {
-    if (currentADCState&1) {
+    if (currentADCState & 1) {
         /* We just did the actual measurement */
-        const uint8_t currentADCChannel = currentADCState>>1;
+        const uint8_t currentADCChannel = currentADCState >> 1;
         rawADCMeasurements[currentADCChannel] = ADC;
         updateOvertemperature();
     }
@@ -104,31 +104,31 @@ void PS9530_Ctrl::updateDAC() {
     case muxChannel_voltage: {
         /* Select voltage channel on MUX */
         PORT_MUX &= ~MASK_MUX;
-        dac_set_unsafe(isStandbyEnabled()?0:rawDACValue[0]);
+        dac_set_unsafe(isStandbyEnabled() ? 0 : rawDACValue[0]);
         currentMuxChannel = muxChannel_current;
         break;
     }
     case muxChannel_current:
         /* Select voltage channel on MUX */
         PORT_MUX |= MASK_MUX;
-        dac_set_unsafe(isStandbyEnabled()?0:rawDACValue[1]);
+        dac_set_unsafe(isStandbyEnabled() ? 0 : rawDACValue[1]);
         currentMuxChannel = muxChannel_voltage;
         break;
     }
 }
 
 void PS9530_Ctrl::startADCCycle() {
-    currentADCState = adcChannel_voltage<<1;
+    currentADCState = adcChannel_voltage << 1;
     startADCConversion();
 }
 
 void PS9530_Ctrl::startADCConversion() {
-    const uint8_t currentADCChannel = currentADCState>>1;
-    if (currentADCChannel<adcChannel__idle) {
+    const uint8_t currentADCChannel = currentADCState >> 1;
+    if (currentADCChannel < adcChannel__idle) {
         /* Set reference voltage to AREF and select channel */
-        ADMUX = (0<<REFS1)|(0<<REFS0)|(currentADCChannel<<MUX0);
+        ADMUX = (0 << REFS1) | (0 << REFS0) | (currentADCChannel << MUX0);
         /* Start the conversion with a clock of fCPU/64 (ca. 125kHz) */
-        ADCSRA = _BV(ADEN)|_BV(ADSC)|_BV(ADIF)|_BV(ADIE)|(1<<ADPS2)|(1<<ADPS1)|(0<<ADPS0);
+        ADCSRA = _BV(ADEN) | _BV(ADSC) | _BV(ADIF) | _BV(ADIE) | (1 << ADPS2) | (1 << ADPS1) | (0 << ADPS0);
     } else {
         /* Deactivate ADC */
         ADCSRA = 0;
@@ -172,7 +172,8 @@ uint16_t PS9530_Ctrl::getMilliAmpsMeasurement() const {
 
 int16_t PS9530_Ctrl::getTemperature1() const {
     int16_t result;
-    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
         result = interpolateADCTemp(tempSensor_1, rawADCMeasurements[adcChannel_temp1]);
     }
     return result;
@@ -180,65 +181,74 @@ int16_t PS9530_Ctrl::getTemperature1() const {
 
 int16_t PS9530_Ctrl::getTemperature2() const {
     int16_t result;
-    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
         result = interpolateADCTemp(tempSensor_2, rawADCMeasurements[adcChannel_temp2]);
     }
     return result;
 }
 
 bool PS9530_Ctrl::isOvertemp() const {
-    return overTempMode!=0;
+    return overTempMode != 0;
 }
 
-const uint16_t PS9530_Ctrl::adcVoltageOffset[32] PROGMEM = {
-     0,  1034,  2068,  3102,  4136,  5170,  6204,  7238,  8272,
-  9306, 10340, 11374, 12408, 13442, 14476, 15510, 16544, 17578,
- 18612, 19646, 20680, 21714, 22748, 23782, 24816, 25850, 26884,
- 27918, 28952, 29986, 31020, 32054
+#define PS9530_VOLTAGE_SHIFT 5
+#define PS9530_CURRENT_SHIFT 5
+const uint16_t PS9530_Ctrl::adcVoltageOffset[32] PROGMEM =
+    {0, 1034, 2068, 3102, 4136, 5170, 6204, 7238, 8272, 9306,
+     10340, 11374, 12408, 13442, 14476, 15510, 16544, 17578, 18612, 19646,
+     20680, 21714, 22748, 23782, 24816, 25850, 26884, 27918, 28952, 29986,
+     31020, 32054};
+const uint16_t PS9530_Ctrl::adcVoltageGradient[32] PROGMEM =
+    {1034, 1034, 1034, 1034, 1034, 1034, 1034, 1034, 1034, 1034,
+     1034, 1034, 1034, 1034, 1034, 1034, 1034, 1034, 1034, 1034,
+     1034, 1034, 1034, 1034, 1034, 1034, 1034, 1034, 1034, 1034,
+     1034, 1034};
+const uint16_t PS9530_Ctrl::adcCurrentOffset[32] PROGMEM =
+    {0, 317, 635, 952, 1269, 1587, 1904, 2221, 2539, 2856,
+     3173, 3491, 3808, 4125, 4443, 4760, 5077, 5395, 5712, 6029,
+     6347, 6664, 6981, 7299, 7616, 7933, 8251, 8568, 8885, 9203,
+     9520, 9838};
+const uint16_t PS9530_Ctrl::adcCurrentGradient[32] PROGMEM =
+    {317, 318, 317, 317, 318, 317, 317, 318, 317, 317,
+     318, 317, 317, 318, 317, 317, 318, 317, 317, 318,
+     317, 317, 318, 317, 317, 318, 317, 317, 318, 317,
+     318, 317};
+const uint16_t PS9530_Ctrl::minTempADC[2] PROGMEM = {548, 485};
+const uint16_t PS9530_Ctrl::maxTempADC[2] PROGMEM = {856, 1194};
+const uint8_t PS9530_Ctrl::shiftTempADC[2] PROGMEM = {5, 6};
+const int16_t PS9530_Ctrl::tempOffset[2][11] PROGMEM = {
+    {150, 123, 101, 81, 61, 41, 21, 1, -19, -41, -55},
+    {150, 116, 91, 70, 50, 32, 16, 1, -14, -28, -41},
 };
-const uint16_t PS9530_Ctrl::adcVoltageGradient[32] PROGMEM = {
-  1034,  1034,  1034,  1034,  1034,  1034,  1034,  1034,  1034,
-  1034,  1034,  1034,  1034,  1034,  1034,  1034,  1034,  1034,
-  1034,  1034,  1034,  1034,  1034,  1034,  1034,  1034,  1034,
-  1034,  1034,  1034,  1034,  1034
+const int16_t PS9530_Ctrl::tempGradient[2][11] PROGMEM = {
+    {-27, -22, -20, -20, -20, -20, -20, -20, -22, -14, 0},
+    {-34, -25, -21, -20, -18, -16, -15, -15, -14, -13, -13},
 };
 
 uint16_t PS9530_Ctrl::interpolateADCVoltage(uint16_t adcValue) {
-    const uint16_t tableIndex = adcValue >> 5;
-    const uint8_t adcRest = adcValue & 31;
+    const uint16_t tableIndex = adcValue >> PS9530_VOLTAGE_SHIFT;
+    const uint8_t adcRest = adcValue & (~((~0)<<PS9530_VOLTAGE_SHIFT));
     const uint16_t base = pgm_read_word(&adcVoltageOffset[tableIndex]);
     const uint16_t gradient = pgm_read_word(&adcVoltageGradient[tableIndex]);
-    return base + ((adcRest * gradient)>>5);
+    return base + ((adcRest * gradient) >> 5);
 }
 
-const uint16_t PS9530_Ctrl::adcCurrentOffset[32] PROGMEM = {
-    0,  317,  635,  952, 1269, 1587, 1904, 2221, 2539, 2856,
- 3173, 3491, 3808, 4125, 4443, 4760, 5077, 5395, 5712, 6029,
- 6347, 6664, 6981, 7299, 7616, 7933, 8251, 8568, 8885, 9203,
- 9520, 9838,
-};
-const uint16_t PS9530_Ctrl::adcCurrentGradient[32] PROGMEM = {
-  317,  318,  317,  317,  318,  317,  317,  318,  317,  317, 
-  318,  317,  317,  318,  317,  317,  318,  317,  317,  318,
-  317,  317,  318,  317,  317,  318,  317,  317,  318,  317,
-  318,  317
-};
-
 uint16_t PS9530_Ctrl::interpolateADCCurrent(uint16_t adcValue) {
-    const uint16_t tableIndex = adcValue >> 5;
-    const uint8_t adcRest = adcValue & 31;
+    const uint16_t tableIndex = adcValue >> PS9530_CURRENT_SHIFT;
+    const uint8_t adcRest = adcValue & (~((~0)<<PS9530_CURRENT_SHIFT));
     const uint16_t base = pgm_read_word(&adcCurrentOffset[tableIndex]);
     const uint16_t gradient = pgm_read_word(&adcCurrentGradient[tableIndex]);
-    return base + ((adcRest * gradient)>>5);
+    return base + ((adcRest * gradient) >> 5);
 }
 
 int16_t PS9530_Ctrl::interpolateADCTemp(uint8_t index, uint16_t adcValue) {
     const uint16_t minADC = pgm_read_word(&minTempADC[index]);
     const uint16_t maxADC = pgm_read_word(&maxTempADC[index]);
     const uint8_t shift = pgm_read_byte(&shiftTempADC[index]);
-    if (adcValue<minADC) {
+    if (adcValue < minADC) {
         adcValue = minADC;
-    } else if (adcValue>maxADC) {
+    } else if (adcValue > maxADC) {
         adcValue = maxADC;
     }
     const uint16_t adcOffset = adcValue - minTempADC[index];
@@ -246,21 +256,8 @@ int16_t PS9530_Ctrl::interpolateADCTemp(uint8_t index, uint16_t adcValue) {
     const uint16_t adcRest = adcOffset - (tableIndex << shift);
     const int16_t base = pgm_read_word(&tempOffset[index][tableIndex]);
     const int16_t gradient = pgm_read_word(&tempGradient[index][tableIndex]);
-    return base + ((adcRest * gradient)>>shift);
+    return base + ((adcRest * gradient) >> shift);
 }
-
-const uint16_t PS9530_Ctrl::minTempADC[2] PROGMEM = { 488, 375 };
-const uint16_t PS9530_Ctrl::maxTempADC[2] PROGMEM = { 821, 1030};
-const uint8_t PS9530_Ctrl::shiftTempADC[2] PROGMEM = { 5, 6 };
-
-const int16_t PS9530_Ctrl::tempOffset[2][11] PROGMEM = {
-    {150, 124, 102,  82,  64,  45,  27,   9,  -9, -28, -47},
-    {150, 112,  84,  60,  39,  21,   4, -11, -26, -39, -52}
-};
-const int16_t PS9530_Ctrl::tempGradient[2][11] PROGMEM = {
-    {-26, -22, -20, -18, -19, -18, -18, -18, -19, -19},
-    {-38, -28, -24, -21, -18, -17, -15, -15, -13, -13}
-};
 
 void PS9530_Ctrl::updateOvertemperature() {
     int16_t temp1 = getTemperature1();
@@ -310,11 +307,11 @@ static inline struct __arduino_savepin __arduino_savepin(uint8_t pin) {
     return savepin;
 }
 
-static inline void __arduino_restorepin(struct __arduino_savepin* savepin) {
+static inline void __arduino_restorepin(struct __arduino_savepin *savepin) {
     digitalWrite(savepin->pin, savepin->value);
 }
 
-#define ARDUINO_SAVEPIN(pin) for (struct __arduino_savepin __save __attribute__((__cleanup__(__arduino_restorepin))) = __arduino_savepin(pin); __save.todo; __save.todo=0)
+#define ARDUINO_SAVEPIN(pin) for (struct __arduino_savepin __save __attribute__((__cleanup__(__arduino_restorepin))) = __arduino_savepin(pin); __save.todo; __save.todo = 0)
 
 ISR(TIMER0_COMPA_vect) {
     /* Wait until any running SPI transaction is done for sure.
@@ -323,7 +320,7 @@ ISR(TIMER0_COMPA_vect) {
      * but would block if no SPI transaction is running and the
      * interrupt flag had been cleared. */
     PORTD |= _BV(PIN7);
-    _delay_loop_2(8*8);
+    _delay_loop_2(8 * 8);
 
     ARDUINO_SAVEPIN(TFT_CS) {
         /* Pull up the TFT_CS pin to deactivate the display connection */
--- firmware/complete/tools/adccalib.py
+++ firmware/complete/tools/adccalib.py
@@ -1,34 +1,143 @@
-# Calculation of basic calibration for ADC conversion
+# Generation of lookup tables for the temperature sensors
 # Copyright (c) 2022, Ralf Gerlich
+import pandas as pd
 import numpy as np
 
+
+def to_c_array(values, formatter=str, colcount=15):
+    # apply formatting to each element
+    values = [formatter(v) for v in values]
+
+    # split into rows with up to `colcount` elements per row
+    rows = [values[i:i+colcount] for i in range(0, len(values), colcount)]
+
+    # separate elements with commas, separate rows with newlines
+    body = ',\n'.join([', '.join(r) for r in rows])
+
+    # assemble components into the complete string
+    return '{%s}' % body
+
+
+# Location of datasheet data
+datasheet_data = '../../../datasheets/PTC_Data.ods'
+# Selected sensor
+sensor = 'KTY81-121'
+# Selected column
+column = 'Rtyp'
+
 # Resistor Values [Ohm]
-Rsense = 0.3 / 6
+Rsense = 0.05
+R54 = 24000
+R56 = 11950
 R60 = 2.7E3
 R61 = 33.E3
 R62 = 100.E3
+R63 = 680
 R64 = 100.E3
+R82 = 2550
 R331 = 22.E3
 R332 = 5.6E3
 
-# Reference voltage [V]
+# Reference voltage on ADC [V]
 vref = 2.5
 
-# Number of steps to generate
-steps = 32
+# Maximum ADC value
+adc_max = 1023
 
-adc_values = np.linspace(start=0, stop=1024, num=steps+1)
+# Number of steps to generate for voltage and current ADC
+voltageADCSteps = 32
+
+# Maximum number of entries in temperature table
+max_temp_entries = 16
+
+# Read raw data
+temp_data_raw = pd.read_excel(datasheet_data, sheet_name=sensor)
+
+# Extract temperature and resistance
+temp_data = temp_data_raw[['Temp_C', column]]
+
+# Calculate values for Temp1
+rTot = R82 + temp_data[column]
+iTot = -5/rTot
+vR82 = iTot * R82
+vTemp1 = -vR82 * R56 / R54
+temp_data['vTemp1'] = vTemp1
+adcTemp1 = np.round(vTemp1 / vref * adc_max).astype(int)
+temp_data['adcTemp1'] = adcTemp1
+
+# Calculate values for Temp2
+rTot = R63 + temp_data[column]
+iTot = 5 / rTot
+vTemp2 = iTot * R63
+temp_data['vTemp2'] = vTemp2
+adcTemp2 = np.round(vTemp2 / vref * adc_max).astype(int)
+temp_data['adcTemp2'] = adcTemp2
+
+dataTemp1 = temp_data.sort_values(by='adcTemp1')
+dataTemp2 = temp_data.sort_values(by='adcTemp2')
+
+# Determine minimum and maxmimum values
+minTempADC = np.min((adcTemp1, adcTemp2), axis=1)
+maxTempADC = np.max((adcTemp1, adcTemp2), axis=1)
+
+# Determine minimum step value
+minTempStep = (maxTempADC-minTempADC)/max_temp_entries
+actTempStepShift = np.ceil(np.log2(minTempStep)).astype(int)
+actTempSteps = np.ceil((maxTempADC-minTempADC)/(1<<actTempStepShift)).astype(int)
+maxTempSteps = np.max(actTempSteps)
+
+# Determine ADC values
+minTempADCTable = minTempADC
+maxTempADCTable = minTempADC + (maxTempSteps<<actTempStepShift)
+tempADCTable = np.linspace(minTempADCTable, maxTempADCTable, maxTempSteps, endpoint=False)
+
+# Interpolate associated temperatures
+temp1Table = np.round(np.interp(tempADCTable[:, 0], dataTemp1['adcTemp1'], dataTemp1['Temp_C']))
+temp2Table = np.round(np.interp(tempADCTable[:, 1], dataTemp2['adcTemp2'], dataTemp2['Temp_C']))
+
+# Determine gradient table
+temp1Gradient = np.diff(temp1Table)
+temp2Gradient = np.diff(temp2Table)
+
+# ADC Values to consider
+adc_values = np.linspace(start=0, stop=adc_max+1, endpoint=True, num=voltageADCSteps+1)
+# Input voltage to the ADC
+voltageIn = adc_values * vref / adc_max
 
 # Voltage calibration
-voltageOffset = np.round(1.E3 * adc_values * vref / 1023 * R62 / R64 * (R60 + R61) / R60).astype(int)
+# Input Voltage on IC5A
+voltageIC5In = voltageIn * R62 / R64
+# Actual voltage on the rails
+voltage = voltageIC5In * (R60 + R61) / R60
+voltageShift=np.ceil(np.log2((adc_max+1)/voltageADCSteps)).astype(int)
+voltageOffset = np.round(1.E3 * voltage).astype(int)
 voltageGradient = np.diff(voltageOffset)
-print(f"voltageShift={np.ceil(np.log2(1024/steps)).astype(int)}")
-print(f"voltageOffset={voltageOffset[:-1]}")
-print(f"voltageGradient={voltageGradient}")
 
 # Current calibration
-currentOffset = np.round(adc_values * vref / 1023 / (1+R331/R332) / Rsense * 1.E3).astype(int)
+# Input on IC312
+voltageIC312In = voltageIn / (1+R331/R332)
+# Actual current sensed
+current = voltageIC312In / Rsense
+currentShift = np.ceil(np.log2(1024/voltageADCSteps)).astype(int)
+currentOffset = np.round(current * 1.E3).astype(int)
 currentGradient = np.diff(currentOffset)
-print(f"currentShift={np.ceil(np.log2(1024/steps)).astype(int)}")
-print(f"currentOffset={currentOffset[:-1]}")
-print(f"currentGradient={currentGradient}")
+
+print("#define PS9530_VOLTAGE_SHIFT %s" % voltageShift)
+print("#define PS9530_CURRENT_SHIFT %s" % currentShift)
+print("const uint16_t PS9530_Ctrl::adcVoltageOffset[%d] PROGMEM = \n%s;" % (voltageADCSteps, to_c_array(voltageOffset[:-1], colcount=10)))
+print("const uint16_t PS9530_Ctrl::adcVoltageGradient[%d] PROGMEM = \n%s;" % (voltageADCSteps, to_c_array(voltageGradient, colcount=10)))
+
+print("const uint16_t PS9530_Ctrl::adcCurrentOffset[%d] PROGMEM = \n%s;" % (voltageADCSteps, to_c_array(currentOffset[:-1], colcount=10)))
+print("const uint16_t PS9530_Ctrl::adcCurrentGradient[%d] PROGMEM = \n%s;" % (voltageADCSteps, to_c_array(currentGradient, colcount=10)))
+
+print("const uint16_t PS9530_Ctrl::minTempADC[2] PROGMEM = %s;" % to_c_array(minTempADC))
+print("const uint16_t PS9530_Ctrl::maxTempADC[2] PROGMEM = %s;" % to_c_array(maxTempADC))
+print("const uint8_t PS9530_Ctrl::shiftTempADC[2] PROGMEM = %s;" % to_c_array(actTempStepShift))
+print("const int16_t PS9530_Ctrl::tempOffset[2][%d] PROGMEM = {" % (maxTempSteps-1));
+print("    %s," % to_c_array(temp1Table[:-1].astype(int)))
+print("    %s," % to_c_array(temp2Table[:-1].astype(int)))
+print("};");
+print("const int16_t PS9530_Ctrl::tempGradient[2][%d] PROGMEM = {" % (maxTempSteps-1));
+print("    %s," % to_c_array(temp1Gradient.astype(int)))
+print("    %s," % to_c_array(temp2Gradient.astype(int)))
+print("};");
