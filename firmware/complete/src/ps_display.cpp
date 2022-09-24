631ae0ac8e02547a0da5be24f79bc7a84e5d52f8
A. Binzxxxxxx <DEvil0000@users.noreply.github.com>
2022-09-25 00:36:26 +0200

    moving constant offset for optimization

--- firmware/complete/src/ps_display.cpp
+++ firmware/complete/src/ps_display.cpp
@@ -403,11 +403,11 @@ void PsDisplay::renderHistory(const uint8_t* history_data, const uint16_t histor
         }
         if (history_data[current_pos] != history_data[last_pos]) {
             for (uint8_t j=0; j < thickness; j++) {
-                tft.writePixel(i, PS_DISPLAY_HEIGHT-history_data[last_pos]+j, DEFAULT_BACKGROUND_COLOR);
+                tft.writePixel(i, 1+PS_DISPLAY_HEIGHT-history_data[last_pos]+j, DEFAULT_BACKGROUND_COLOR);
             }
             if (i > 1) { //not the best workaround for the sticky edge due to wrap and missing old data
                 for (uint8_t j=0; j < thickness; j++) {
-                    tft.writePixel(i, PS_DISPLAY_HEIGHT-history_data[current_pos]+j, color_override != 0x1337 ? color_override : TOOLBOX_LOGO_LIGHT_RED);
+                    tft.writePixel(i, 1+PS_DISPLAY_HEIGHT-history_data[current_pos]+j, color_override != 0x1337 ? color_override : TOOLBOX_LOGO_LIGHT_RED);
                 }
             }
         }
@@ -564,22 +564,22 @@ void PsDisplay::setCentiWattsLimit(const int16_t watts_limit) {
 void PsDisplay::setMilliVolts(const int16_t voltage) {
     this->milli_volts = voltage;
     history_volts_pos = (history_volts_pos+1) % HISTORY_LENGTH;
-    // normalize to (PS_DISPLAY_HEIGHT-(PT18_IN_PXH*2+6*2))+1 => ~ 187 pixel
-    history_volts[history_volts_pos] = (voltage >> 8) * 1.589f +1;
+    // normalize to (PS_DISPLAY_HEIGHT-(PT18_IN_PXH*2+6*2)) => ~ 187 pixel
+    history_volts[history_volts_pos] = (voltage >> 8) * 1.589f;
 }
 
 void PsDisplay::setMilliAmps(const int16_t amps) {
     this->milli_amps = amps;
     history_apms_pos = (history_apms_pos+1) % HISTORY_LENGTH;
-    // normalize to (PS_DISPLAY_HEIGHT-(PT18_IN_PXH*2+6*2))+1 => ~ 187 pixel
-    history_amps[history_apms_pos] = (amps >> 6) * 1.192f +1;
+    // normalize to (PS_DISPLAY_HEIGHT-(PT18_IN_PXH*2+6*2)) => ~ 187 pixel
+    history_amps[history_apms_pos] = (amps >> 6) * 1.192f;
 }
 
 void PsDisplay::setCentiWatts(const int16_t watts) {
     this->centi_watts = watts;
     history_watts_pos = (history_watts_pos+1) % HISTORY_LENGTH;
-    // normalize to (PS_DISPLAY_HEIGHT-(PT18_IN_PXH*2+6*2))+1 => ~ 187 pixel
-    history_watts[history_watts_pos] = (watts >> 8) * 1.589f +1;
+    // normalize to (PS_DISPLAY_HEIGHT-(PT18_IN_PXH*2+6*2)) => ~ 187 pixel
+    history_watts[history_watts_pos] = (watts >> 8) * 1.589f;
 }
 
 void PsDisplay::setCurser(const row_t row, const uint8_t pos) {
